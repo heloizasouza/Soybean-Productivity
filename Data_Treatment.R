afca70f171b0d80b62ea83d3d35f0bcd59f80278
Heloiza Oliveira <heloiza.oliveira.souza@gmail.com>
2024-04-11 13:10:46 -0300

    WIP

--- Data_Treatment.R
+++ Data_Treatment.R
@@ -4,7 +4,7 @@
 
 library(xlsx)
 library(tidyverse)
-
+library(sp)
 
 
 # Data Treatment ----------------------------------------------------------
@@ -14,34 +14,42 @@ main_data <- read.xlsx("Data/Dados - Produtividade em Latossolo e Plintossolo - 
 main_data <- main_data[,1:12]
 
 soybean_data <- main_data |>
+  # removing accents
   # removendo acentos
-  mutate_at(vars(Local, Textura.do.solo, Cultivar), ~stringi::stri_trans_general(.,"Latin-ASCII")) |>
+  mutate_at(vars(Local, Textura.do.solo, Cultivar), 
+            ~stringi::stri_trans_general(.,"Latin-ASCII")) |>
+  # transforming categorical variables
   # transformando variáveis categóricas
   mutate_at(vars(Local,Ano,Solo,Cultivar, Textura.do.solo), as.factor) |>
+  #transforming the Date covariates
   # transformando as covariáveis de Data
   mutate_at(vars(Plantio), as.Date) |>
-  mutate(Colheita = Plantio+Ciclo) |>
-  separate(Lat..e.Long., into = c("Latit", "Longit"), sep = ";")
+  mutate(Colheita = Plantio+Ciclo)
 
 # correcting coordinates
-soybean_data <- soybean_data %>%
+soybean_data <- soybean_data |>
+  separate(Lat..e.Long., into = c("Latit", "Longit"), sep = ";") |>
   mutate(Latit = case_when(
     Local == "Pium" ~ "10°12' 58'' S",
     Local == "Paraiso do Tocantins" ~ "10°11' 16.9'' S",
+    Local == "Pedro Afonso" ~ "08°58' 03'' S",
+    Local == "Aparecida do Rio Negro" ~ "09°57' 07'' S",
+    Local == "Lagoa da Confusao" ~ "10°47' 37'' S",
     TRUE ~ as.character(Latit)
   ),
   Longit = case_when(
     Local == "Pium" ~ "49°15' 1.4'' W",
     Local == "Paraiso do Tocantins" ~ "48°40' 54.6'' W",
+    Local == "Goiania" ~ "49°30' 11'' W",
+    Local == "Pedro Afonso" ~ "48°10' 29'' W",
+    Local == "Aparecida do Rio Negro" ~ "47°58' 19'' W",
+    Local == "Lagoa da Confusao" ~ "49°37' 25'' W",
     TRUE ~ as.character(Longit)
-  ))
-
-
-
+  )) |>
   mutate(Latitude = char2dms(from = Latit,chd = "°", chm = "'", chs = "''") 
          |> as.numeric()) |>
   mutate(Longitude = char2dms(from = Longit,chd = "°", chm = "'", chs = "''")
-         |> as.numeric()) |>
+         |> as.numeric())
   mutate(Longitude = -Longitude)
 
 
